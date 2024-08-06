1492fba0d146f724823e9b370e991446d38e512a
omaaro7 <omarwafeek00@outlook.com>
2024-08-07 02:25:20 +0300

    commmit

--- assets/js/settings/client/general/siteinfo.js
+++ assets/js/settings/client/general/siteinfo.js
@@ -28,57 +28,10 @@ async function change_global_settings() {
     }
     site_name.textContent = data[0].site_name;
     site_logo.firstElementChild.src = `${path}`;
-    theme(data[0].site_style)
 
   }
 }
-async function theme(s) {
-  let root = document.querySelector(":root");
-  console.log(root
-  );
-  
-  let dark = {                                           
-    "--background-color": "#080c14",
-  "--main-color": "#111827",
-  "--titles-color": "white",
-  "--second-color": "#7d0633",
-  "--thered-color": "rgb(31, 34, 38)",
-  "--fourth-color": "#2465f0",
-  "--five-color": "#ba9544",
-  "--six-color": "#4e3434",
-  "--seven-color": "#5c5470",
-  "--eight-color": "#1e76c4"              
-    
-  }
-  let light= {
-      "--background-color": "#eceff4",
-      "--main-color": "#f3f4f6",
-      "--titles-color": "rgb(1, 1, 1)",
-      "--second-color": "#ff2949",
-      "--thered-color": "rgb(156, 163, 175)",
-      "--fourth-color": "#6abff7",
-      "--five-color": "#791aa5",
-      "--six-color": "#ff9100",
-      "--seven-color": "#a600ff",
-      "--eight-color": "#1e76c4"
-    };
-    
-  if (s == 0) {
 
-    for (const [key,value] of Object.entries(light)) {
-      root.style.setProperty(`${key}`,`${value}`)
-      
-      console.log(key,value);
-      
-    }
-  }else{
-    for (const [key,value] of Object.entries(dark)) {
-      root.style.setProperty(`${key}`,`${value}`)
-      console.log(key,value);
-      
-    }
-  }
-  }
   
 
 change_global_settings();
